06f995bbbe9b12e089065dc4b7bcfdfd8d7e60df
“Miguel <erqmac@gmail.com>
2025-03-06 12:30:08 -0800

    forgot to push Mb Mar6

--- src/scenes/PlayIntro3.js
+++ src/scenes/PlayIntro3.js
@@ -4,6 +4,10 @@ class PlayIntro3 extends Phaser.Scene {
     }
 
     create() {
+
+        //THIS WILL BE THE WHERE MARK WALKS INTOO THE ELEVATOR AND BEFORE GOING IN 
+        //SAYS HELLO TO THE SECURITY GUARD
+        //will be implemented later 
         
         let video = this.add.video(640, 360, 'PlayIntro3').setOrigin(0.5, 0.5);
         video.play(true);
--- src/scenes/PlayMain.js
+++ src/scenes/PlayMain.js
@@ -5,14 +5,56 @@ class PlayMain extends Phaser.Scene {
 
     create() {
 
+        //load PlayMain assets & sounds
+        const  map = this.add.tilemap("map")
+        const tileset = map.addTilesetImage("Room_Builder_Office_32x32","gameTileset")    //adjust to tilesheet name when i get it
+        const walkableLayer = map.createLayer("Floors", gameTileset)
+        const wallLayer = map.createLater("Walls", gameTileset)
+
+        //PlayMain load objects
+
+        //Player character sprite and animations
+        this.player = this.physics.add.sprite(x,y, 'playerIdle')
+
+        this.anims.create({
+            key:'idle',
+            frames: this.anims.generateFrameNumbers('playerIdle', {start: 0, end:5}),
+            frameRate:10,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key:'walk',
+            frames: this.anims.generateFrameNumbers('playerWalk', {start: 0, end:5}),
+            frameRate:12,
+            repear: -1
+        })
+
+        this.player.play('idle')
 
 
+        //add camera and collidable walls 
 
 
-
+        //add player movement and controls
+        this.cursors = this.input.keyboard.createCursorKeys()
     }
 
     update() {
+
+        //player movement and controls
+        if(this.cursors.left.isDown){
+            this.player.setVelocityX(-160)
+            this.player.play('walk',true)
+            this.player.flipX = true;
+        }else if(this.cursors.right.isDown){
+            this.player.setVelocityX(160)
+            this.player.play('walk',true)
+            this.player.flipX = false;
+        }else{
+            this.player.setVelocityX(0)
+            this.player.play('idle',true)
+        }
 
     }
 
