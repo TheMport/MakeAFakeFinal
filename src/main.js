9fcf401fb44e62af6184ca323b70d73435b2abb7
helloimjennie <jenniferle@mail.com>
2025-03-18 23:36:46 -0700

    Credit Scene
    Music Credit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // Name: Jennie Le & Miguel Comonfort
 // Game: Severance: Get To The OTC!
 // Completion Time: 40 Hours
+
 // Components used in this scene:
 // Physics system (for movement and collisions)
 // Animation manager (for sprite animations)
@@ -12,6 +13,11 @@
 // Camera (for following the player) 
 // Tweens (for animations and transitions) 
 // Time (for tracking game time) 
+
+// Music Credit: Free sounds from Pixabay.com 
+// https://www.youtube.com/watch?v=lBzegiTr1Ao&list=PL32bE3Gvp-kTMQMJ1lz9_2AgLiOt_YYLr&index=2&ab_channel=TheodoreShapiro-Topic
+// https://www.youtube.com/watch?v=i2cU70DLj1k&ab_channel=MrGrappl
+
 'use strict'
 
 let config = {
